df19071cbea61bff907ff67499b27dfb783cd7d8
qwrwed <qwrwed@users.noreply.github.com>
2022-02-12 03:11:37 +0000

    write processed json files

--- convert.js
+++ convert.js
@@ -1,7 +1,18 @@
-var glob = require( 'glob' )
-  , path = require( 'path' );
+const
+  glob = require( 'glob' ),
+  path = require( 'path' ),
+  jsonfile = require('jsonfile')
 
-glob.sync( './src/**/*.json' ).forEach( function( file ) {
-  var f = require(file);
-  console.log(file, Object.keys(f))
+const inputDir = "./input";
+const outputDir = "./output";
+
+// https://stackoverflow.com/a/28976201
+glob.sync( `${inputDir}/**/*.json` ).forEach( function( inputFilePath ) {
+  const fileName = inputFilePath.split("/").at(-1);
+  const outputFilePath = `${outputDir}/${fileName}`;
+  const f = require(inputFilePath);
+  console.log(`writing`, outputFilePath)
+  jsonfile.writeFile(outputFilePath, f, { spaces: 4 }, function (err) {
+    if (err) console.error(err)
+  })
 });
