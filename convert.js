2c3abe4bc8715dd023ce1261b81ae7f9c7a8aaf8
qwrwed <qwrwed@users.noreply.github.com>
2022-02-12 03:32:29 +0000

    add reqs in script

--- convert.js
+++ convert.js
@@ -3,15 +3,31 @@ const
   path = require( 'path' ),
   jsonfile = require('jsonfile')
 
+const verbose = false;
 const inputDir = "./input";
 const outputDir = "./output";
 
 // https://stackoverflow.com/a/28976201
 glob.sync( `${inputDir}/**/*.json` ).forEach( function( inputFilePath ) {
   const fileName = inputFilePath.split("/").at(-1);
+  const apiType = fileName.split(".").at(0);
   const outputFilePath = `${outputDir}/${fileName}`;
   const f = require(inputFilePath);
-  console.log(`writing`, outputFilePath)
+  let schemaCount = 0;
+  // console.log("processing", inputFilePath, "...");
+  if (f.components.schemas){
+    for (const schemaId in f.components.schemas){
+      const schema = f.components.schemas[schemaId];
+      if ((schema.type === "object") && (schema.properties)){
+        const reqs = Object.keys(schema.properties)
+        if (verbose) console.log(`adding ${reqs.length} required fields to`, apiType, schemaId);
+        schema.required = reqs;
+        schemaCount++;
+      }
+    }
+  }
+
+  console.log(`saving`, outputFilePath, `(added reqs to ${schemaCount} schemas)`)
   jsonfile.writeFile(outputFilePath, f, { spaces: 4 }, function (err) {
     if (err) console.error(err)
   })
